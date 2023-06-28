3698049a1c00f2c833be3d4b024327d14ce97474
Andre Rodrigues <andre.rodrigues@paytrix.io>
2023-06-28 11:45:26 +0100

    feat(about): add about page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,8 +11,8 @@ module.exports = {
       },
       fontFamily: {
         sans: ['var(--aw-font-sans)', ...defaultTheme.fontFamily.sans],
-        serif: ['var(--aw-font-serif)', ...defaultTheme.fontFamily.serif],
-        heading: ['var(--aw-font-heading)', ...defaultTheme.fontFamily.sans],
+        serif: ['Lusitana'],
+        heading: ['Playfair Display SC'],
       },
     },
   },
