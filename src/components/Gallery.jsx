37df328d531a75ca59aebe9f9c64883d8e9bf841
Andre Rodrigues <andre.rodrigues@paytrix.io>
2023-06-29 13:48:24 +0100

    feat(schedule): add schedule section and react

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Gallery() {
+  return (
+    <div class="section">
+      <h2
+        data-delay={700}
+        data-animation="fade-in"
+        class="animation text-4xl md:text-5xl leading-tighter tracking-wider my-9 md:mt-0 font-heading text-red-800 dark:text-orange-200"
+      >
+        Galeria
+      </h2>
+    </div>
+  );
+}
+
+export default Gallery;
