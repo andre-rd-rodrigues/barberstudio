3f396c8bd2c71c93491c981ab94c676e9dd4ec1d
Andre Rodrigues <andre.rodrigues@paytrix.io>
2023-07-02 22:56:25 +0100

    fix: add copywrite and parallax style

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,35 +6,37 @@ import 'slick-carousel/slick/slick-theme.css';
 const Products = () => {
   const products = [
     {
-      title: 'Product 5',
+      title: 'Styling Pomade',
       description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum explicabo fugiat aliquid minima. Magnam laboriosam dolor ut. Corporis nihil molestias, ipsa veniam sint facere.',
+        'Achieve effortless style with our versatile pomade. Provides a strong hold and a natural shine for all-day sophistication.',
       price: '29.99',
       src: 'https://images.unsplash.com/photo-1655563096407-42bf664e9b9f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
     },
     {
-      title: 'Product 1',
+      title: 'Nourishing Shampoo',
       description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum explicabo fugiat aliquid minima. Magnam laboriosam dolor ut. Corporis nihil molestias, ipsa veniam sint facere.',
+        'Revitalize your hair with our nourishing shampoo. Infused with essential oils, it cleanses and hydrates, leaving your hair feeling soft and refreshed.',
       price: '9.99',
       src: 'https://images.unsplash.com/photo-1663025042626-6f0d925c6b59?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1064&q=80',
     },
     {
-      title: 'Product 2',
-      description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum explicabo fugiat aliquid minima.',
+      title: 'Styling Pomade',
+      description:
+        'Achieve effortless style with our versatile pomade. Provides a strong hold and a natural shine for all-day sophistication.',
       price: '14.99',
       src: 'https://images.unsplash.com/photo-1655394009750-b6cfc0b9455f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1064&q=80',
     },
     {
-      title: 'Product 3',
-      description: 'Lorem, ipsum dolor sit amet.',
+      title: 'Beard Oil',
+      description:
+        'Tame your beard with our premium beard oil. Nourishes and moisturizes, promoting a healthy, well-groomed appearance while adding a subtle, irresistible fragrance.',
       price: '19.99',
       src: 'https://images.unsplash.com/photo-1655394009794-df4f7cd8582a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=930&q=80',
     },
     {
-      title: 'Product 4',
+      title: 'Nourishing Shampoo',
       description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum explicabo fugiat aliquid minima. Magnam.',
+        'Revitalize your hair with our nourishing shampoo. Infused with essential oils, it cleanses and hydrates, leaving your hair feeling soft and refreshed.',
       price: '24.99',
       src: 'https://images.unsplash.com/photo-1655241406148-93853da337ca?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80',
     },
@@ -100,6 +102,12 @@ const Products = () => {
           <h2 className="text-4xl md:text-5xl leading-tighter tracking-wider my-9 md:mt-0 font-heading text-red-800 dark:text-orange-200">
             Produtos
           </h2>
+          <p className="font-serif leading-8 text-lg text-justify delay-200 fill-mode-both mb-10">
+            At Barber Studio, we understand the importance of quality hair products for maintaining your style beyond
+            the chair. We proudly offer a curated selection of premium hair products to help you achieve and maintain
+            the look you desire. Explore our handpicked collection of top-quality products and experience the
+            difference.
+          </p>
           <Slider className="max-w-6xl mx-auto" {...settings}>
             {products.map((product, i) => (
               <ProductCard key={i} product={product} />
